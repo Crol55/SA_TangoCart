01c3add8d1f392477dce5269a8ed158ffacc10ed
GermanMZ <1683298400101@ingeniera.usac.edu.gt>
2021-08-02 14:34:41 -0600

    docs: create

---
+++ frontend/tangoCart/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BsNavbarComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/tangoCart/src/app/login/login.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  
+  form = new FormGroup({
+    email:    new FormControl('',Validators.required), 
+    password: new FormControl('',Validators.required),
+  })
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+ get email(){
+   return this.form.get("email")
+ }
+
+ get password(){
+   return this.form.get('password')
+ }
+
+ login(){
+     console.log(this.form.value)
+     if(this.form.valid){
+      this.router.navigate(['/'])
+     }else{
+      this.form.setErrors({
+        invalidLogin: true
+    });
+     }
+ }
+
+}
