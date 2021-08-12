972a5e08a3da9aa1f8399497f17dc3971a803c51
Crol55 <carlosorantes77@gmail.com>
2021-08-12 16:49:50 -0600

    Creacion de microservicio para proveedores en el puerto 4200

---
+++ microservicio-authProveedor/src/routes/rutas.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+const controlador = require('../controllers/controlador');
+
+var router = express.Router(); // Para manejar los 'Endopoints' a los que respondera este microservicio 
+
+router.get('/test', (req, res)=>{ res.send('Todo bien'); });
+
+router.post('/login', controlador.login);
+
+router.post('/verify', controlador.verify);
+
+module.exports = router;
