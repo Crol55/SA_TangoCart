56c324b1a85338d9f86330fb048d84e592bd4520
Crol55 <carlosorantes77@gmail.com>
2021-08-23 13:12:53 -0600

    testeando cambio en docker-compose

--- microservicio-authProveedor/index.js
+++ microservicio-authProveedor/index.js
@@ -6,6 +6,7 @@ const Mongo_DB = require('./src/DB/Mongo_DB');
 
 const change = "Este string solo para testear que hubieron cambios en las imagenes de docker"
 const change2 = "Este string solo para testear que hubieron cambios en las imagenes de docker"
+const change3 = "Este string solo para testear que hubieron cambios en las imagenes de docker"
 const puerto = process.env.PORT || 4200;
 
 Mongo_DB.connectDB();
