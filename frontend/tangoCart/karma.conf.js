0f6d40121edb6362d39bae2efe1e21701682dd11
GermanMZ <1683298400101@ingeniera.usac.edu.gt>
2021-09-06 13:18:56 -0600

    update

--- frontend/tangoCart/karma.conf.js
+++ frontend/tangoCart/karma.conf.js
@@ -32,12 +32,23 @@ module.exports = function (config) {
         { type: 'text-summary' }
       ]
     },
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--no-sandbox',
+          '--disable-gpu',
+          '--headless',
+          '--remote-debugging-port=9222'
+        ]
+      }
+    },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: false,
     restartOnFileChange: true
   });
